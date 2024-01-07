f9d198c2e26cb722ab7d8f6969dac151a1b654e4
Stephen Kelman <stephenrkelman@gmail.com>
2024-01-07 11:09:59 -0800

    produces links to individual pokemon pages

--- README.md
+++ README.md
@@ -2,4 +2,9 @@
 
 ## References
 [Web Scraping](https://realpython.com/beautiful-soup-web-scraper-python/)
+
 [Installing Python Modules on Windows](https://docs.python.org/3/installing/index.html)
+
+[Python Regex String Methods](https://www.w3schools.com/python/python_regex.asp)
+
+[Python map()](https://www.w3schools.com/python/ref_func_map.asp)
--- scraper.py
+++ scraper.py
@@ -1,5 +1,5 @@
 import requests
-
+import re
 from bs4 import BeautifulSoup
 
 URL = "https://pokemondb.net/pokedex/national"
@@ -7,3 +7,9 @@ page = requests.get(URL)
 
 soup = BeautifulSoup(page.content, "html.parser")
 
+# print(soup)
+print(type(soup))
+
+pokemon_links = re.findall("href=\"\/pokedex\/[^\"]*\"", str(soup))
+pokemon_links = list(map(lambda href_link : "pokemondb.net"+href_link[6:-1], pokemon_links))
+print(pokemon_links)
