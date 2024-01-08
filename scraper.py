f1fd141de260a72d04b7435f750abd3cad44889c
Stephen Kelman <stephenrkelman@gmail.com>
2024-01-08 11:12:04 -0800

    able to extract pokemon names and types

--- scraper.py
+++ scraper.py
@@ -2,14 +2,37 @@ import requests
 import re
 from bs4 import BeautifulSoup
 
-URL = "https://pokemondb.net/pokedex/national"
-page = requests.get(URL)
+def get_pokemon_links():
+    URL = "https://pokemondb.net/pokedex/national"
+    page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, "html.parser")
-# print(soup)
+    soup = BeautifulSoup(page.content, "html.parser")
+    # print(soup)
 
-pokemon_links = re.findall("<a class=\"ent-name\" href=\"\/pokedex\/[^\"]*\"", str(soup))
-pokemon_links = list(map(lambda href_link : "https://pokemondb.net"+href_link[26:-1], pokemon_links))
-# print(pokemon_links)
+    pokemon_links = re.findall("<a class=\"ent-name\" href=\"\/pokedex\/[^\"]*\"", str(soup))
+    pokemon_links = list(map(lambda href_link : "https://pokemondb.net"+href_link[26:-1], pokemon_links))
+    # print(pokemon_links)
 
-pokemon_links = list(set(pokemon_links))
+    return list(set(pokemon_links))
+
+def get_pokemon_info(pokemon_links):
+    for URL in pokemon_links:
+        page = requests.get(URL)
+        soup = BeautifulSoup(page.content, "html.parser")
+        name = soup.find_all("h1")[0].text
+        print(name)
+        dex_data = soup.find_all(
+            "h2", 
+            # class_="grid-col span-md-6 span-lg-4",
+            string="Pokédex data"
+        )[0].parent
+        type_tags = dex_data.find_all(
+            "a",
+            class_="type-icon"
+        )
+        types = [type_tag.text for type_tag in type_tags]
+        print(types)
+        return
+
+pokemon_links = get_pokemon_links()
+get_pokemon_info(pokemon_links)
