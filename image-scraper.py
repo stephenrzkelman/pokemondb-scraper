f4e2478f82e9e952c556c7af3b1357a3c9bb8542
Stephen Kelman <stephenrkelman@gmail.com>
2024-01-19 17:32:28 -0800

    full sprite coverage, except for ash-greninja

--- image-scraper.py
+++ image-scraper.py
@@ -13,20 +13,30 @@ def scrape_images():
         if os.path.exists(mon_image_file):
             # skip if we already have the image
             continue
-        generation_names = ["black-white", "x-y", "sun-moon", "sword-shield", "scarlet-violet"]
+        generation_names = [
+            "black-white", 
+            "x-y", 
+            "sun-moon", 
+            "sword-shield", 
+            "scarlet-violet", 
+            "ultra-sun-ultra-moon",
+            "omega-ruby-alpha-sapphire/dex"
+        ]
         def generation_url(generation_name):
             return f"https://img.pokemondb.net/sprites/{generation_name}/normal/{id}.png"
         generation_responses = list(map(
             lambda gen: requests.get(generation_url(gen)),
             generation_names
         ))
+        found = False
         for response in generation_responses:
             if response.status_code == 200:
                 with open(mon_image_file, "wb+") as image_file:
                     image_file.write(response.content)
-                continue
-        # print(f"{mon} ({id}) sprite not found in gen 5-9 main games")
-        record_file.write(f"{mon} ({id})\n")
+                found = True
+                break
+        if not found:
+            record_file.write(f"{mon} ({id})\n")
 
 
 scrape_images()
