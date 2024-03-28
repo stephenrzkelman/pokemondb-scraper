1bb3048a7ed1afa9b247f38c93da2185e9fc46d0
Stephen Kelman <stephenrkelman@gmail.com>
2024-03-28 14:25:10 -0700

    scraper for moves of a specific pokemon, no special variant handling

---
+++ scrapers/poke-move-scraper.py
@@ -0,0 +1,26 @@
+import requests
+from bs4 import BeautifulSoup
+
+def scrape_moves(pokemon_page):
+    move_section = pokemon_page.find(
+        "div",
+        class_ = "tabset-moves-game"
+    )
+    move_chunks = move_section.find_all(
+        "td",
+        class_ = "cell-name"
+    )
+    moves = list(map(
+        lambda move_chunk: move_chunk.find(
+            "a",
+            class_ = "ent-name"
+        ).text,
+        move_chunks
+    ))
+    distinct_moves = list(set(moves))
+    return moves
+
+
+page = requests.get("https://pokemondb.net/pokedex/gothita")
+soup = BeautifulSoup(page.content, "html.parser")
+print(scrape_moves(soup))
